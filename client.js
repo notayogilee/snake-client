0fb4faa35bb2724bc79caf089abedbcd3a3da5c7
Lee Castelani <notayogilee@gmail.com>
2020-02-12 15:24:32 -0500

    updated

--- client.js
+++ client.js
@@ -6,6 +6,11 @@ const connect = function () {
   });
 
   conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    console.log('Name: LRC');
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
--- play.js
+++ play.js
@@ -1,3 +1,6 @@
 const net = require('net');
 const { connect } = require('./client');
 
+// server.on('connection', (client) => {
+//   console.log("New client connected!");
+// })
