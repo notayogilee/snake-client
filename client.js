3f8b23e3b581d85ad4490867e6d59b877234c8a4
Lee Castelani <notayogilee@gmail.com>
2020-02-12 18:19:34 -0500

    a

--- client.js
+++ client.js
@@ -8,15 +8,34 @@ const connect = function () {
   conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-    console.log('Name: LRC');
-  });
+    conn.write('Name: LRC');
+    setInterval(() => {
+      conn.write('Move: up');
 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
+    }, 1000)
+    conn.write('data', (data) => {
+      // console.log(data);
+      if (data === '')
+        setInterval(() => {
+          conn.write('Move: left');
+        }, 1000)
+    })
+    //   if (data === 'w')
+    //     setInterval(() => {
+    //       conn.write('Move: up');
+    //     }, 1000)
+    //   if (data === 'a')
+    //     setInterval(() => {
+    //       conn.write('Move: left');
+    //     }, 1000)
+    //   if (data === 'f')
+    //     setInterval(() => {
+    //       conn.write('Move: right');
+    //     }, 1000)
+    // });
   });
 
   return conn;
 }
-
 console.log('Connecting...');
 connect();
--- play.js
+++ play.js
@@ -1,6 +1,3 @@
 const net = require('net');
 const { connect } = require('./client');
 
-// server.on('connection', (client) => {
-//   console.log("New client connected!");
-// })
