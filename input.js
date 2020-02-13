efe29d5d3bff3163ba9ebf1082382b812cc3af0d
Lee Castelani <notayogilee@gmail.com>
2020-02-12 19:31:44 -0500

    d

---
+++ input.js
@@ -0,0 +1,20 @@
+
+const handleUserInput = (key) => {
+  console.log('exit');
+  if (key === '\u0003') {
+    process.exit()
+  }
+}
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', function (key) {
+    handleUserInput(key);
+  })
+
+  return stdin;
+}
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,27 +1,9 @@
 const net = require('net');
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const handleUserInput = (key) => {
-  console.log('exit');
-  if (key === '\u0003') {
-    process.exit()
-  }
-}
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', function (key) {
-    handleUserInput(key);
-  })
-
-  return stdin;
-}
 
 console.log('Connecting...');
 connect();
+
+setupInput();
