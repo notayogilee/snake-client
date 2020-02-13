4a86736805ce5382abacc304d71f6d6bdbddbcd6
Lee Castelani <notayogilee@gmail.com>
2020-02-12 20:19:08 -0500

    e

--- input.js
+++ input.js
@@ -1,18 +1,35 @@
+//stores the active TCP connection objects
+let connection;
 
-const handleUserInput = (key) => {
-  console.log('exit');
+const handleUserInput = ('data', key => {
+
+  if (key === 'w') {
+    console.log('up');
+    connection.write('Move: up');
+  }
+  if (key === 's') {
+    console.log('down');
+    connection.write('Move: down');
+  }
+  if (key === 'a') {
+    console.log('left');
+    connection.write('Move: left');
+  }
+  if (key === 'd') {
+    console.log('right');
+    connection.write('Move: right');
+  }
   if (key === '\u0003') {
     process.exit()
   }
-}
-const setupInput = function () {
+})
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', function (key) {
-    handleUserInput(key);
-  })
+  stdin.on('data', handleUserInput)
 
   return stdin;
 }
